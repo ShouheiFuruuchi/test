96747c7ba7792729f3be199bc09a609c38562af0
ShouheiFuruuchi <87424554+ShouheiFuruuchi@users.noreply.github.com>
2021-09-20 01:18:59 +0900

    test1

---
+++ test.py
@@ -0,0 +1,9 @@
+import selenium
+
+from selenium import webdriver
+
+
+#driver = webdriver.Chrome("/Usr/local/bin/chromedriver")
+driver = webdriver.Chrome("/Users/furuuchishouhei/Downloads/chromedriver-2")
+
+driver.get("https://www.google.com/")
